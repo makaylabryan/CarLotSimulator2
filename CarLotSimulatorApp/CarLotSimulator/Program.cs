d5ba8ed8f78f56f4082b67d5e3dbc66cafa3b753
Makayla Bryan <makaylabryan@gmail.com>
2024-03-21 14:35:35 -0400

    add static method

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -73,6 +73,8 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars  ===DONE
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list. ==DONE
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console ===DONE
+
+           
         }
     }
 }
